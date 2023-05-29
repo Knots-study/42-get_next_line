590c6f188d08aa5064727d99ec08e7a04b3fabcc
Knots <y.karaki.732@ms.saitama-u.ac.jp>
2023-05-29 14:02:07 +0900

    [fix] ヘッダーファイルにBUFFER_SIZEを追加

--- get_next_line.h
+++ get_next_line.h
@@ -17,6 +17,10 @@
 # include <fcntl.h>
 # include <unistd.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
+
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
